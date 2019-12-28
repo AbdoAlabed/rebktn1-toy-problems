1d969e20cc98635773fd6c830d6344cba116fcc9
AbdoAlabed <henry_3030@icloud.com>
2019-12-28 00:55:41 -0800

    done!

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -7,3 +7,29 @@ evenOccurrence([ "cat", "dog", "dig", "cat" ]) // "cat"
 evenOccurrence([[1,2,3], [1,2], [1,2,3]]) // [1,2,3]
 evenOccurrence([{a: 1, b: 2}, {a:1, b:3}, {a:1, b:2}]) // {a:1, b:2}
 */
+function evenOccurrence(element){
+    var obj = {};
+    var count = 0;
+      for (var i = 0; i < element.length; i++) {
+    	if(obj.hasOwnProperty(element[i])){
+    		obj[element[i]] = obj[element[i]]+ 1
+    	}else{
+    		count = 0;
+    		obj[element[i]] = ++count;
+    	} 
+    }
+    
+    for(var key in obj){
+    	
+    	if(obj[key] % 2 === 0){
+    		if(parseInt(key) === Number)
+    		return parseInt(key);
+    }
+    	else{
+    		return key;
+    	}
+    	
+    }
+    return null;
+}
+evenOccurrence([ 1, 3, 3, 3, 2, 4, 4, 2, 5 ])
