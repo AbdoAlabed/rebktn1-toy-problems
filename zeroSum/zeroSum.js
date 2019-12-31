b1f332d303be3bfe52407ab71e125f044c175e7f
AbdoAlabed <henry_3030@icloud.com>
2019-12-31 00:44:13 -0800

    done!

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -4,6 +4,23 @@ Given an array of negative/positive integers, return true if there exists two nu
 
 Constraints: Solve this linear time complexity O(n)
 
-zeroSum([ 1, 3, 2, -3 ]) // true
+zeroSum([ 1, 2, 3, -1,-2]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+function zeroSum(numbers){
+    var obj = {};
+    for(var i = 0;  i < numbers.length ; i++){
+
+          if(obj[Math.abs(numbers[i])] === undefined){
+              obj[Math.abs(numbers[i])] = numbers[i]
+          }
+          else{
+              return true;
+          }
+    }
+    return false;
+}
+zeroSum([ 1, 3, 2, -3])
+zeroSum([ -3, 1, 3, 2 ])
+zeroSum([ 1, 2, 3, -1,-2])
+zeroSum([ 5, 7, 2, 9 ])
