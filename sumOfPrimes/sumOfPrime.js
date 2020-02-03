99870e3f909bc17b05aee7b957194ac62e59aeb9
AbdoAlabed <henry_3030@icloud.com>
2020-02-03 02:27:17 -0800

    work

--- sumOfPrimes/sumOfPrime.js
+++ sumOfPrimes/sumOfPrime.js
@@ -4,5 +4,30 @@ Given a positive number, return the sum of all positive primes that are less tha
 */
 
 function sumOfPrimes(n) {
-    // your code here...
+  // your code here...
+  let sumPositivePrimes = 0;
+  let primeTester = number => {
+    if (number === 1) return false;
+    let numberTest = [2, 3, 4, 5, 6, 7, 8, 9];
+    let primeNumber = true;
+    for (let i = 0; i < numberTest.length; i++) {
+      if (n === numberTest[i] || n % numberTest[i] !== 0) {
+        continue;
+      } else {
+        primeNumber = false;
+      }
+    }
+    return primeNumber;
+  };
+
+  if (n < 2) return "this number is either negitive number or less than 2";
+  else {
+    for (let i = 2; i <= n; i++) {
+      if (primeTester(i)) {
+        sumPositivePrimes += i;
+        if (sumPositivePrimes > n) return sumPositivePrimes - n;
+      }
+    }
+  }
+  return sumPositivePrimes;
 }
