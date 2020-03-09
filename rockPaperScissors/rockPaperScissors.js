46a623e0477beee5443bc888bf3197e283d96c5b
AbdoAlabed <henry_3030@icloud.com>
2020-03-09 15:08:19 +0200

    work!! latest updates !

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -7,6 +7,11 @@
 *    "RRP",
 *    "RRS",
 *    "RPR",
+	 "RSR",
+	 "RPP",
+	 "RSS",
+	 "RPS",
+	 "RSP"
 *    ...etc...
 *   ]
 *
@@ -18,10 +23,58 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-	for (var i = 0; i < Things.length; i++) {
-		Things[i]
-	}
-  // TODO: your solution here
+// var rockPaperScissors = function(number) {
+//   let rock = "R",
+//     peper = "P",
+//     scissor = "S";
+//   let test = "RPS";
+//   let temp1 = "",
+//     temp2 = "",
+//     temp3 = "";
+//   let result = [];
+//   for (var i = 0; i < number; i++) {
+//     if (i === 0) {
+//       temp1 = rock;
+//       temp2 = peper;
+//       temp3 = scissor;
+//     } else if (i === 1) {
+//       temp1 = peper;
+//       temp2 = rock;
+//       temp3 = scissor;
+//     } else {
+//       temp1 = scissor;
+//       temp2 = rock;
+//       temp3 = peper;
+//     }
+
+//     for (var j = 0; j < number * number; j++) {
+//       if (j < number) {
+//         result.push(temp1 + temp1 + test[j]);
+//       } else if (j < number + number) {
+//         result.push(temp1 + temp2 + test[j - number]);
+//       } else if (j < number * number) {
+//         result.push(temp1 + temp3 + test[j - (number + number)]);
+//       }
+//     }
+//   }
+//   console.log(result);
+//   // TODO: your solution here
+// };
+
+//
+var rockPaperScissors = function(number) {
+  if (number === 0) return [];
+  const result = [];
+
+  function playRound(plays) {
+    if (plays.length === number) {
+      result.push(plays);
+      return;
+    }
+    ["R", "P", "S"].forEach(play => {
+      playRound(plays + play);
+    });
+  }
+  playRound("");
+  return result;
 };
